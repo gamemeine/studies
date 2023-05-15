12ab9f97b51d44c6402f5e60107deba2b29b7e1f
gamemeine <grzegorz.statkiewicz@gmail.com>
2023-05-15 22:46:39 +0200

    Add docs

--- arko/x86/remnth/remnth.h
+++ arko/x86/remnth/remnth.h
@@ -1,6 +1,7 @@
 #ifndef _REMNTH_H
 #define _REMNTH_H
 
+// take n-th character from s
 char* remnth(char* s, int n);
 
 #endif
--- arko/x86/removerng/removerng.h
+++ arko/x86/removerng/removerng.h
@@ -1,6 +1,7 @@
 #ifndef _F_H
 #define _F_H
 
+// remove characters from s inbetween a and b
 char* removerng(char *s, char a, char b);
 
 #endif
