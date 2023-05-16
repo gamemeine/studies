685cd7c29e3ba6c5bb1f8e5697aa5020642f1f4a
gamemeine <grzegorz.statkiewicz@gmail.com>
2023-05-16 18:34:52 +0200

    Refactor

--- arko/x86/increasing/increasing.s
+++ arko/x86/increasing/increasing.s
@@ -6,30 +6,28 @@ increasing:
     mov ebp, esp
 
 begin:
-    mov ecx, [ebp + 8]  ; load first char from s
-    mov eax, [ebp + 8]  ; load first char from output
+    mov ecx, [ebp + 8]  ; load first char's address from s
+    mov eax, [ebp + 8]  ; load first char's address from output
 
-    mov dh, 0           ; get previous char
+    xor dh, dh           ; get previous char
 
 next:
     mov dl, [ecx]   ; get next char from s
-    cmp dl, 0       ; check if end
+    test dl, dl     ; check if end
     jz end
 
     cmp dl, dh      ; check if previous code is bigger
     jge write
 
-    mov dh, dl      ; update previoys char
+prepare:
+    mov dh, dl      ; update previous char
     inc ecx         ; go to next char
     jmp next
 
 write:
     mov byte [eax], dl   ; write current char
     inc eax         ; increment output
-
-    mov dh, dl      ; update previoys char
-    inc ecx         ; go to next char
-    jmp next
+    jmp prepare
     
 end:
     mov byte [eax], 0   ; end ouput
